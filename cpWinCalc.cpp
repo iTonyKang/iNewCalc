9e9f7b83258db94e89b8062ef1c533649a042b80
iTonyKang <itonykang@gmail.com>
2023-08-25 13:31:00 -0400

    update on 2023082r5 1:29PM

--- cpWinCalc.cpp
+++ cpWinCalc.cpp
@@ -147,7 +147,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: Add any drawing code that uses hdc here...
-            Rectangle(hdc, 20, 20, 200, 100);
+            Rectangle(hdc, 20, 20, 600, 500); //20230825 1:29pm
             EndPaint(hWnd, &ps);
         }
         break;
